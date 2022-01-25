26b43e4c494694d4489d536c7ac199e70d09531b
Chloe Rees <reeschloe@yahoo.com>
2022-01-24 20:15:39 -0600

    finished reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+  const splitArray = str.split("")
+  let output = ''
+  for (i = (splitArray.length - 1); i >= 0; i--) {
+      output += splitArray[i]    
+  }
+  return(output)  
 };
 
 // Do not edit below this line
