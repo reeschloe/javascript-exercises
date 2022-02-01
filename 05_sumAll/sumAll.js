fa452fb9a7c991579130eae1e432418accbdbc2d
Chloe Rees <reeschloe@yahoo.com>
2022-01-31 21:13:25 -0600

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  let value = 0
+  if (!(Number.isInteger(start) && start>0 && Number.isInteger(end) && end>0)) {
+      return('ERROR')
+  }
+  else if (start <= end) {
+    for (i = start; i<= end; i++) {
+      value += i;  
+    }
+    return(value)
+  }
+  else if (start > end) {
+    for (i = end; i<= start; i++) {
+        value += i;  
+    }
+    return(value)
+  }
 };
+
+function isPositiveInteger(num1,num2) {
+    if (Number.isInteger(num1) && num1 > 0 && Number.isInteger(num2) && num2 > 0) {
+        return('true')
+    }
+    else {
+        return('false')
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
