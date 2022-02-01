bcacc6eee23c2e1290f4da668b5bece015f1a218
Chloe Rees <reeschloe@yahoo.com>
2022-01-31 20:44:21 -0600

    finished fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+  let array = arguments[0]
+  for (i = 1; i <= (arguments.length - 1); i++) {
+    value = arguments[i];
+    for (j = 0; j <= (array.length - 1); j++) {
+        if (array[j] === value) {
+            array.splice(j, 1);
+        }
+    }
+  }
+  return(array)
 };
 
 // Do not edit below this line
